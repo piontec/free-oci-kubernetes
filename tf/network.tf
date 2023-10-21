0e72744cbe2000a06d2bc859c57b9a75a159f3a1
Łukasz Piątkowski <piontec@gmail.com>
2023-10-21 15:31:09 +0200

    turn off auto sec groups management and move them to tf; remove redundant sec-groups rules in TF

--- tf/network.tf
+++ tf/network.tf
@@ -46,43 +46,10 @@ resource "oci_core_security_list" "public_subnet_sl" {
   display_name = "free-k8s-public-subnet-sl"
 
   egress_security_rules {
-    stateless        = false
     destination      = "0.0.0.0/0"
     destination_type = "CIDR_BLOCK"
     protocol         = "all"
-  }
-  egress_security_rules {
-    destination      = "10.0.1.0/24"
-    destination_type = "CIDR_BLOCK"
-    protocol         = "6"
     stateless        = false
-
-    tcp_options {
-      max = 10256
-      min = 10256
-    }
-  }
-  egress_security_rules {
-    destination      = "10.0.1.0/24"
-    destination_type = "CIDR_BLOCK"
-    protocol         = "6"
-    stateless        = false
-
-    tcp_options {
-      max = 32322
-      min = 32322
-    }
-  }
-  egress_security_rules {
-    destination      = "10.0.1.0/24"
-    destination_type = "CIDR_BLOCK"
-    protocol         = "6"
-    stateless        = false
-
-    tcp_options {
-      max = 32757
-      min = 32757
-    }
   }
 
   ingress_security_rules {
@@ -91,16 +58,7 @@ resource "oci_core_security_list" "public_subnet_sl" {
     source_type = "CIDR_BLOCK"
     protocol    = "all"
   }
-  ingress_security_rules {
-    stateless   = false
-    source      = "0.0.0.0/0"
-    source_type = "CIDR_BLOCK"
-    protocol    = "6"
-    tcp_options {
-      min = 6443
-      max = 6443
-    }
-  }
+
   ingress_security_rules {
     protocol    = "6"
     source      = "0.0.0.0/0"
