460340890b4b4f3fc454063c01109162de739d40
Łukasz Piątkowski <piontec@gmail.com>
2025-10-22 12:31:22 +0200

    upgrade kubernetes to 1.34

--- tf/variables.tf
+++ tf/variables.tf
@@ -22,14 +22,39 @@ variable "arm_pool_images" {
   type        = list(string)
   description = "ready images for ARM pools"
   default = [
-    # "Oracle-Linux-8.10-aarch64-2025.05.19-0-OKE-1.33.1-804",
-    "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaxg25d2ph37ruwvqdwxfj2eskxwkgvgbxzgrdg7q6a4ajvbx7u55q",
-    # "Oracle-Linux-8.10-aarch64-2025.05.19-0-OKE-1.33.1-804",
-    "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaxg25d2ph37ruwvqdwxfj2eskxwkgvgbxzgrdg7q6a4ajvbx7u55q",
+    # Oracle-Linux-8.10-aarch64-2025.08.31-0-OKE-1.34.1-1191
+    "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaawafhb7vmsk5lyxiohqvopofvpylynmq7m6ww3nzuglmpbydcrcfa",
+    # Oracle-Linux-8.10-aarch64-2025.08.31-0-OKE-1.34.1-1191
+    "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaawafhb7vmsk5lyxiohqvopofvpylynmq7m6ww3nzuglmpbydcrcfa",
   ]
 }
 variable "enable_wireguard" {
   type        = bool
   description = "Weather or not create wireguard security group allow rules."
   default     = true
+}
+
+variable "git_path" {
+  description = "Path to the cluster manifests in the Git repository"
+  type        = string
+  nullable    = false
+  default     = "flux/"
+}
+
+variable "git_ref" {
+  description = "Git branch or tag in the format refs/heads/main or refs/tags/v1.0.0"
+  type        = string
+  default     = "refs/heads/main"
+}
+
+variable "flux_version" {
+  description = "Flux version semver range"
+  type        = string
+  default     = "2.x"
+}
+
+variable "flux_registry" {
+  description = "Flux distribution registry"
+  type        = string
+  default     = "ghcr.io/fluxcd"
 }
